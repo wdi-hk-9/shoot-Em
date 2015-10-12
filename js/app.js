853e8e8a9eb52b5120b408e6458a9e39912301c3
Toni Anne Romero <tonianne.romero@gmail.com>
2015-10-12 21:48:56 +0800

    player bounderies: tried to set max&min border height: semi-fail

--- js/app.js
+++ js/app.js
@@ -1,40 +1,68 @@
 $(function(){
+
 var game = new Game();
+var keys = [];    //to detect multiple keys
+var playerMovement = 20;
+var player = $('#player');
+//var enemeyPosition = $('.enemy').position();
 
-//var positionPlayer = $('#player').position();
-var xPosPlayer = $('#player').offset().top
-var yPosPlayer = $('#player').offset().left
+function shoot(){
+  var xPos = player.position().top;
+  $('div#box').append('<div class="bullet"></div>');  //insert bullet
+  $('.bullet').css({'top': xPos+25 + 'px'}).animate({left: '490px'}, 1200); //attach bullet to player position
 
-var enemeyPosition = $('.enemy').position();
-var htmlBullet = '<div class="bullet"></div>';
-var htmlEnemy = '<div id="enemy"></div>';
+    //add: push to array to tract bullets
+    //add: after spacebar, release player position so bullet keeps own position
+    //add: if collides with enemy position OR goes out of the window, splice out of array. toggle off/disapear/hide
+}
 
+//bullet boundery
+  // function bulletBoundery(){
+  //   if ($('.bullet').left == 490 ){
+  //     // remove from array (splice)
+  //   }
+  // }
 
-  function shoot(){
-    $('div#fireArea').append(htmlBullet);
-    $('.bullet').animate({left: '465px'}, 1200); //move bullet from left to right. max 440px
-      //follow player
-      //if collides with enemy position, toggle off/disapear/hide
+//I can't make it work. trying to set it to the border limit
+function boundery() {
+  if (player.position().top >= 200){
+    player.animate({top: ='200px'});
   }
+  if (player.position().top <= 0){
+    player.animate({top: ='10px'});
+  };
+}
 
+//changed to if statement. easier for me to see.
+function keysPressed(e) {
+  // store an entry for every key pressed
+  keys[e.keyCode] = true;
 
-  $(document).keydown(function playerKeyMove(e){
-   switch(e.keyCode){
-    case 38: //up arrow key
-      $('#player').animate({top: '-=20px'}, 200).dequeue();
-      break;
+  //spacebar
+  if (keys[32]) {
+    shoot();
+    e.preventDefault();
+  }
+  //up arrow key
+  if (keys[38]) {
+    player.animate({top: '-=' + playerMovement}, 100);
+    e.preventDefault();
+  }
+  //down arrow key
+  if (keys[40]) {
+    player.animate({top: '+=' + playerMovement}, 100);
+    e.preventDefault();
+  }
+  boundery();
+}
 
-    case 40: //down arrow key
-      $('#player').animate({top: '+=20px'}, 200).dequeue();
-      break;
+//once released, same keys in arrays become false.
+function keysReleased(e) {
+    keys[e.keyCode] = false;
+}
 
-    case 32: //space bar
-      shoot();
-      break;
-      // this needs to work even though 38 and 40 are pressed
-    }
-  }).keyup(function(e){ //end of keydown.
-
-  })
+//event listener
+$(document).keyup(keysReleased);
+$(document).keydown(keysPressed);
 
 }); //end of doc.ready
